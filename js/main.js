dffe4c20166147ce0a890ff97f04382dcba8cf85
Morois <ehot.tohet@gmail.com>
2022-02-19 14:12:39 +0300

    Add randomizers

--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@ getRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-
-
 getCoordinate = (min, max, afterPoint) => {
   if (min < 0 || max <= min) {
     return false;
