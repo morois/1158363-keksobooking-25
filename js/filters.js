6fe93aee3cf1a0472553a85719f182ad31dc8395
Morois <ehot.tohet@gmail.com>
2022-04-18 21:01:11 +0300

    commit

---
+++ js/filters.js
@@ -0,0 +1,6 @@
+const mapFilters = document.querySelector('.map__filters');
+
+const typeFilter = mapFilters.querySelector('#housing-type');
+const priceFilter = mapFilters.querySelector('#housing-price');
+const roomsFilter = mapFilters.querySelector('#housing-rooms');
+const guestsFilter = mapFilters.querySelector('#housing-guests');
